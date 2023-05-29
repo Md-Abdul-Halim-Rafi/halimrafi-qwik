8b41544b43594a0d7f9d3230b82e7e0582c4c9b1
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@gmail.com>
2023-05-29 09:55:46 +0600

    added the origin

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,49 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { useLocation } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  return (
-    <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
-    </>
-  );
+
+	const location = useLocation();
+	
+	return (
+		<div
+			style={{
+				height: "100%",
+				display: "flex",
+				alignItems: "center",
+				flexDirection: "column",
+				justifyContent: "center",
+			}}
+		>
+			<h1>Hi 👋</h1>
+			<p>
+				Can't wait to see what you build with qwik!
+				<br />
+				Happy coding.
+			</p>
+
+			<p 
+				style={{ 
+					color: "#fff", 
+					padding: "2px 4px", 
+					borderRadius: "2px",
+					width: "fit-content", 
+					backgroundColor: "#000", 
+				}}
+			>
+				{location.url.origin}
+			</p>
+		</div>
+	);
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-  meta: [
-    {
-      name: 'description',
-      content: 'Qwik site description',
-    },
-  ],
+	title: "Welcome to Dynamic Qwik",
+	meta: [
+		{
+			name: "description",
+			content: "Qwik site description",
+		},
+	],
 };
